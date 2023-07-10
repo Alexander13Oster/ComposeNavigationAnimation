036eaf85f477130cde19d227fcca4b5dd4365cb4
Alexander Oster <alexander13oster@gmail.com>
2023-07-10 20:51:27 +0200

    libs upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,27 +43,27 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4"
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
 
-    implementation("androidx.compose.ui:ui:1.5.0-beta01")
-    implementation("androidx.navigation:navigation-compose:2.7.0-beta02")
+    implementation("androidx.compose.ui:ui:1.5.0-beta03")
+    implementation("androidx.navigation:navigation-compose:2.7.0-beta01")
 
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta01")
-    implementation("androidx.compose.material:material:1.5.0-beta01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta03")
+    implementation("androidx.compose.material:material:1.5.0-beta03")
 
     implementation("androidx.compose.material3:material3:1.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0-beta01")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0-beta01")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0-beta01")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0-beta03")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0-beta03")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0-beta03")
 }
